e9c1c983c524b6f6f7ccd493e3c0749611ab7913
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2015-06-23 21:56:16 +0300

    Scaffold presentation website

---
+++ SKeepyStorefrontWebSite/SKeepyStorefrontWebSite/App_Start/BundleConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SKeepyStorefrontWebSite
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                        "~/Scripts/jquery.min.js",
+                        "~/Scripts/jquery.dropotron.min.js",
+                        "~/Scripts/jquery.scrollex.min.js",
+                        "~/Scripts/jquery.scrolly.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/home").Include(
+                        "~/Scripts/skel.min.js",
+                        "~/Scripts/util.js",
+                        "~/Scripts/main.js"
+                        ));
+
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                "~/Content/font-awesome.min.css",
+                "~/Content/main.css"
+                ));
+        }
+    }
+}
