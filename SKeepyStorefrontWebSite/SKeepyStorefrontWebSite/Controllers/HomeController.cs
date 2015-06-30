684ad67e5c68ef2f760a7ab9d0a7118b0de8bd56
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2015-06-30 17:14:22 +0300

    Remove auth

--- SKeepyStorefrontWebSite/SKeepyStorefrontWebSite/Controllers/HomeController.cs
+++ SKeepyStorefrontWebSite/SKeepyStorefrontWebSite/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
-﻿using SKeepyStorefrontWebSite.Auth;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SKeepyStorefrontWebSite.Controllers
 {
-    [BasicAuthenticationAttribute("hintee", "Password#1", BasicRealm = "Hintee")]
     public class HomeController : Controller
     {
         public ActionResult Index()
